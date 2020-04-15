653a0dff3a2ffbc6df791ca499d75fdac183fc10
Vishavjeet6 <vishavjeetsinghthakur@gmail.com>
2020-04-15 13:20:21 +0530

    adding server file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const { db } = require('./db')
+const taskRoute = require('./routes/tasks')
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use('/', express.static(__dirname + '/public'))
+app.use('/tasks', taskRoute)
+
+db.sync({alter: true})
+  .then(() => {
+    app.listen(6534)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
